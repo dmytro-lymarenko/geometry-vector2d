78d5cbb813736506bb7a3f49aec9832c64b59865
Dmytro <dmytro.lymarenko@gmail.com>
2016-01-21 22:08:55 +0200

    fix tests

--- tests/setup.js
+++
@@ -1,5 +0,0 @@
-var jsdom = require('jsdom');
-
-var doc = global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
-global.window = document.defaultView;
-global.navigator = {userAgent: 'node.js'};
